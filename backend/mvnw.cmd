53958d6952772e7912df80719d4529eff2c07a50
AnhHao2505 <nguyenanhhao2555@gmail.com>
2026-08-22 12:30:37 +0700

    chore(docker): update multi-platform Hub images and cross-platform setup files for Windows & Mac

--- backend/mvnw.cmd
+++ backend/mvnw.cmd
@@ -7,7 +7,7 @@
 @REM "License"); you may not use this file except in compliance
 @REM with the License.  You may obtain a copy of the License at
 @REM
-@REM    http://www.apache.org/licenses/LICENSE-2.0
+@REM    https://www.apache.org/licenses/LICENSE-2.0
 @REM
 @REM Unless required by applicable law or agreed to in writing,
 @REM software distributed under the License is distributed on an
@@ -18,26 +18,91 @@
 @REM ----------------------------------------------------------------------------
 
 @REM ----------------------------------------------------------------------------
-@REM Maven Start Up Batch Script
-@REM
-@REM Required ENV vars:
-@REM JAVA_HOME - Location of a JDK home directory
-@REM
+@REM Maven Start Up Batch script
 @REM ----------------------------------------------------------------------------
 
-@echo off
-if "%USERPROFILE%" == "" goto errorUserprofile
-set "MAVEN_PROJECTBASEDIR=%~dp0"
-if "%MAVEN_PROJECTBASEDIR:~-1%" == "\" set "MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR:~0,-1%"
+@if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BATCH_ECHO%
+@if "%MAVEN_BATCH_PAUSE%" == "on" set MAVEN_BATCH_PAUSE=on
 
-set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
-set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
+@setlocal
 
-java "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" -classpath %WRAPPER_JAR% %WRAPPER_LAUNCHER% %*
+set ERROR_CODE=0
+
+@REM To isolate internal variables from possible post scripts, we use another setlocal
+@setlocal
+
+@REM ==== START VALIDATION ====
+if not "%JAVA_HOME%" == "" goto OkJHome
+
+echo.
+echo ERROR: JAVA_HOME not found in your environment. >&2
+echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo location of your Java installation. >&2
+echo.
+goto error
+
+:OkJHome
+if exist "%JAVA_HOME%\bin\java.exe" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory. >&2
+echo JAVA_HOME = "%JAVA_HOME%" >&2
+echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo location of your Java installation. >&2
+echo.
+goto error
+
+@REM ==== END VALIDATION ====
+
+:init
+
+@REM Find the project base dir, i.e. the dir that contains the folder ".mvn".
+@REM Fallback to current dir if not found.
+
+set MAVEN_PROJECTBASEDIR=%MAVEN_BASEDIR%
+if "%MAVEN_PROJECTBASEDIR%"=="." set MAVEN_PROJECTBASEDIR=
+if not "%MAVEN_PROJECTBASEDIR%"=="" goto valBase
+
+set MAVEN_PROJECTBASEDIR=%CD%
+
+:findBaseDir
+if exist "%MAVEN_PROJECTBASEDIR%\.mvn" goto valBase
+set "MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR%\.."
+if not "%MAVEN_PROJECTBASEDIR%"=="..\" goto findBaseDir
+set MAVEN_PROJECTBASEDIR=%CD%
+
+:valBase
+set "EXEC_DIR=%CD%"
+set "WDIR=%MAVEN_PROJECTBASEDIR%"
+
+@REM Set WRAPPER_JAR
+set "WRAPPER_JAR=%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
+set "WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain"
+
+@REM Download Wrapper Jar if not present
+if exist "%WRAPPER_JAR%" goto runEscaped
+
+set MAVEN_WRAPPER_PROPERTIES="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.properties"
+powershell -Command "& { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object Net.WebClient).DownloadFile((Get-Content '%MAVEN_WRAPPER_PROPERTIES%' | Select-String -Pattern 'wrapperUrl=(.*)' | ForEach-Object { $_.Matches.Groups[1].Value }), '%WRAPPER_JAR%') }"
+
+:runEscaped
+"%JAVA_HOME%\bin\java.exe" %MAVEN_OPTS% -classpath "%WRAPPER_JAR%" "-Dmaven.home=%MAVEN_HOME%" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" %WRAPPER_LAUNCHER% %*
+if ERRORLEVEL 1 goto error
 goto end
 
-:errorUserprofile
-echo ERROR: USERPROFILE environment variable not set.
-exit /b 1
+:error
+set ERROR_CODE=1
 
 :end
+@endlocal & set ERROR_CODE=%ERROR_CODE%
+
+if not "%MAVEN_SKIP_RC%" == "" goto skipRcPost
+if exist "%HOME%\mavenrc_post.bat" call "%HOME%\mavenrc_post.bat"
+if exist "%HOMEDRIVE%%HOMEPATH%\mavenrc_post.bat" call "%HOMEDRIVE%%HOMEPATH%\mavenrc_post.bat"
+:skipRcPost
+
+if "%MAVEN_BATCH_PAUSE%" == "on" pause
+
+if "%NDS_PAUSE%" == "on" pause
+
+cmd /C exit /B %ERROR_CODE%
